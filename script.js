08734e979bf515afc654e6542d92fd7cf6874acd
carodriguez404 <carodriguez404@gmail.com>
2025-08-11 22:06:27 -0500

    segundo

---
+++ script.js
@@ -0,0 +1,11 @@
+let listaDecompras={
+    yuca: 5,
+    arracacha: 10,
+    papa: 34,
+    arroz: 40,
+}
+
+for(lista1 in listaDecompras){
+    console.log(lista1);
+    console.log(listaDecompras[lista1]);
+}
